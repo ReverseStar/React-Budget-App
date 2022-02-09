bb60325c27d66eb6c976f1d2391f5a108852b33e
ReverseStar <destro.mahmud@gmail.com>
2022-02-09 22:23:29 +0600

    created and designed acards.Next to contexts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Stack } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import BudgetCard from "./components/BudgetCard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Container className="my-4">
+            <Stack direction="horizontal" gap="2" className="mb-4">
+                <h1 className="me-auto">Budget App</h1>
+                <Button variant="primary">Add Budget</Button>
+                <Button variant="outline-primary">Add Expenses</Button>
+            </Stack>
+
+            <div
+                style={{
+                    display: "grid",
+                    gridTemplateColumns:
+                        "repeat(auto-fill,minmax(350px, 1fr))",
+                    gap: "1rem",
+                    alignItems: "flex-start",
+                }}
+            >
+                <BudgetCard
+                    name="Entertainment"
+                    gray
+                    amount={400}
+                    max={1000}
+                ></BudgetCard>
+            </div>
+        </Container>
+    );
 }
 
 export default App;
