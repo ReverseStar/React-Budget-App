ace71cfcebd66f5d6c0e5ccab156afbf96261acc
ReverseStar <destro.mahmud@gmail.com>
2022-02-13 18:16:57 +0600

    context and modal

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 import { Button, Stack } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
+import AddBudgetModal from "./components/AddBudgetModal";
 import BudgetCard from "./components/BudgetCard";
 
 function App() {
     return (
-        <Container className="my-4">
-            <Stack direction="horizontal" gap="2" className="mb-4">
-                <h1 className="me-auto">Budget App</h1>
-                <Button variant="primary">Add Budget</Button>
-                <Button variant="outline-primary">Add Expenses</Button>
-            </Stack>
+        <>
+            <Container className="my-4">
+                <Stack direction="horizontal" gap="2" className="mb-4">
+                    <h1 className="me-auto">Budget App</h1>
+                    <Button variant="primary">Add Budget</Button>
+                    <Button variant="outline-primary">Add Expenses</Button>
+                </Stack>
 
-            <div
-                style={{
-                    display: "grid",
-                    gridTemplateColumns:
-                        "repeat(auto-fill,minmax(350px, 1fr))",
-                    gap: "1rem",
-                    alignItems: "flex-start",
-                }}
-            >
-                <BudgetCard
-                    name="Entertainment"
-                    gray
-                    amount={400}
-                    max={1000}
-                ></BudgetCard>
-            </div>
-        </Container>
+                <div
+                    style={{
+                        display: "grid",
+                        gridTemplateColumns:
+                            "repeat(auto-fill,minmax(350px, 1fr))",
+                        gap: "1rem",
+                        alignItems: "flex-start",
+                    }}
+                >
+                    <BudgetCard
+                        name="Entertainment"
+                        gray
+                        amount={400}
+                        max={1000}
+                    ></BudgetCard>
+                </div>
+            </Container>
+            <AddBudgetModal show />
+        </>
     );
 }
 
--- src/contexts/BudgetsContext.js
+++ src/contexts/BudgetsContext.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from "react";
-import {v4 as uuidV4} from "uuid";
+import { v4 as uuidV4 } from "uuid";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 const BudgetsContext = React.createContext();
 
@@ -8,23 +9,39 @@ export function useBudgets() {
 }
 
 export const BudgetsProvider = ({ children }) => {
-
-    const [budgets, setBudgets] = useState([]);
-    const [expenses, setExpense] = useState([]);
+    const [budgets, setBudgets] = useLocalStorage('budgets',[]);
+    const [expenses, setExpenses] = useLocalStorage('expenses', []);
 
     function getBudgetExpenses(budgetId) {
-        return expenses.filter(expense => expense.budgetId === budgetId)
+        return expenses.filter((expense) => expense.budgetId === budgetId);
     }
 
-    function addExpense() {}
+    function addExpense({description, amount, budgetId}) {
+        setExpenses((prevExpenses) => {
+            return [...prevExpenses, ...{ id: uuidV4(), description, amount, budgetId}];
+        });
+    }
 
-    function addBudget({name, max}) {
+    function addBudget({ name, max }) {
+        setBudgets((prevBudgets) => {
+            if (prevBudgets.find((budget) => budget.name === name)) {
+                return prevBudgets;
+            }
+            return [...prevBudgets, { id: uuidV4(), name, max }];
+        });
+    }
+
+    function deleteBudget({id}) {
         setBudgets(prevBudgets => {
-            return [...prevBudgets, {id: uuidV4(), name, max}]
+            return prevBudgets.filter(budget => budget.id !== id)
         })
     }
-    function deleteBudget() {}
-    function deleteExpense() {}
+
+    function deleteExpense({id}) {
+        setExpenses(prevExpenses => {
+            return prevExpenses.filter(expense => expense.id !== id)
+        })
+    }
 
     return (
         <BudgetsContext.Provider
